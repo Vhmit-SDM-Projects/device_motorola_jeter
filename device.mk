4dc065541ff0b61e4a50bfb70a898554659a6621
Vhmit <praxedes.9630@gmail.com>
2023-05-14 11:11:42 -0300

    jeter: Build MotoActions
    * Its path is already defined in the script
    
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- device.mk
+++ device.mk
@@ -207,10 +207,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # MotoActions
-ifeq ($(filter %ahannah %rhannah,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     MotoActions
-endif
 
 # OMX
 PRODUCT_PACKAGES += \
